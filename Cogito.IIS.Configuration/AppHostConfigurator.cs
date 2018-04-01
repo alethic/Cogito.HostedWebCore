138f486755d74a9b138f88b093e1254650359799
Jerome Haltom <wasabi@cogito.cx>
2018-04-01 15:24:07 -0500

    Added a subconfigure method.

--- Cogito.IIS.Configuration/AppHostConfigurator.cs
+++ Cogito.IIS.Configuration/AppHostConfigurator.cs
@@ -37,6 +37,17 @@ namespace Cogito.IIS.Configuration
         public XElement Element => element;
 
         /// <summary>
+        /// Applies additional configure to the <see cref="AppHostConfigurator"/>.
+        /// </summary>
+        /// <param name="configure"></param>
+        /// <returns></returns>
+        public AppHostConfigurator Configure(Action<AppHostConfigurator> configure = null)
+        {
+            configure?.Invoke(this);
+            return this;
+        }
+
+        /// <summary>
         /// Configures a site.
         /// </summary>
         /// <param name="path"></param>
