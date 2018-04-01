c7da860edde1261da3968a74bcdb4ac5e191a807
Jerome Haltom <wasabi@cogito.cx>
2018-04-01 18:34:56 -0500

    +semver: patch Addd GlobalModule support

---
+++ Cogito.IIS.Configuration/AppHostGlobalModuleConfigurator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+using Cogito.Web.Configuration;
+
+namespace Cogito.IIS.Configuration
+{
+
+    public class AppHostGlobalModuleConfigurator : IWebElementConfigurator
+    {
+
+        readonly XElement element;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="element"></param>
+        public AppHostGlobalModuleConfigurator(XElement element)
+        {
+            this.element = element ?? throw new ArgumentNullException(nameof(element));
+        }
+
+        /// <summary>
+        /// Returns the configuration.
+        /// </summary>
+        /// <returns></returns>
+        public XElement Element => element;
+
+        public AppHostGlobalModuleConfigurator Clear()
+        {
+            Element.RemoveNodes();
+            return this;
+        }
+
+        public AppHostGlobalModuleConfigurator Remove(string moduleName)
+        {
+            Element.Elements().Where(i => (string)i.Attribute("name") == moduleName).Remove();
+            return this;
+        }
+
+        public AppHostGlobalModuleConfigurator Add(string moduleName, string image)
+        {
+            Element.Add(
+                new XElement("add",
+                    new XAttribute("name", image),
+                    new XAttribute("image", image)));
+            return this;
+        }
+
+    }
+
+}
---
+++ Cogito.IIS.Configuration/WebSystemWebServerConfiguratorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Cogito.Web.Configuration;
+
+namespace Cogito.IIS.Configuration
+{
+
+    public static class WebSystemWebServerConfiguratorExtensions
+    {
+
+        public static WebSystemWebServerConfigurator GlobalModules(this WebSystemWebServerConfigurator self, Action<AppHostGlobalModuleConfigurator> configurator = null)
+        {
+            return self.Configure("globalModules", e => configurator?.Invoke(new AppHostGlobalModuleConfigurator(e)));
+        }
+
+    }
+
+}
