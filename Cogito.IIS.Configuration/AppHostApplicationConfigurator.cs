c6dc102de91327573b0fe88b579e09fbe12c363f
Jerome Haltom <jhaltom@fileandserve.com>
2019-09-07 00:08:47 -0500

    Add some more settings.

--- Cogito.IIS.Configuration/AppHostApplicationConfigurator.cs
+++ Cogito.IIS.Configuration/AppHostApplicationConfigurator.cs
@@ -28,6 +28,57 @@ namespace Cogito.IIS.Configuration
         public XElement Element => element;
 
         /// <summary>
+        /// Helper method to set an attribute value.
+        /// </summary>
+        /// <param name="attributeName"></param>
+        /// <param name="attributeValue"></param>
+        /// <returns></returns>
+        AppHostApplicationConfigurator SetAttributeValue(string attributeName, string attributeValue)
+        {
+            return this.Configure(e => e.SetAttributeValue(attributeName, attributeValue));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public AppHostApplicationConfigurator Path(string path)
+        {
+            return SetAttributeValue("path", path);
+        }
+
+        /// <summary>
+        /// Sets the value of the 'preloadEnabled' attribute.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public AppHostApplicationConfigurator PreloadEnabled(bool? value)
+        {
+            return SetAttributeValue("preloadEnabled", value != null ? ((bool)value ? "true" : "false") : null);
+        }
+
+        /// <summary>
+        /// Sets the value of the 'serviceAutoStartEnabled' attribute.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public AppHostApplicationConfigurator ServiceAutoStartEnabled(bool? value)
+        {
+            return SetAttributeValue("serviceAutoStartEnabled", value != null ? ((bool)value ? "true" : "false") : null);
+        }
+
+        /// <summary>
+        /// Sets the value of the 'serviceAutoStartProvider' attribute.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public AppHostApplicationConfigurator ServiceAutoStartProvider(string value)
+        {
+            return SetAttributeValue("serviceAutoStartProvider", value);
+        }
+
+        /// <summary>
         /// Configures a virtual directory.
         /// </summary>
         /// <param name="path"></param>
